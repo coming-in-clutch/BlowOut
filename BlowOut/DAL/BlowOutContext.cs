40272074f9d712be0f633a660118acb5657d9864
Brittany Rowe <rowebrittany@gmail.com>
2017-11-14 16:38:39 -0700

    Everything before scaffolding (I think)

---
+++ BlowOut/DAL/BlowOutContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BlowOut.Models;
+using System.Data.Entity;
+
+namespace BlowOut.DAL
+{
+    public class BlowOutContext : DbContext
+    {
+        public BlowOutContext(): base("BlowOutContext")
+        {
+
+        }
+
+        public DbSet<Instrument> Instruments { get; set; }
+        public DbSet<Client> Clients { get; set; }
+    }
+}

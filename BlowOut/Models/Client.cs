83f71a54aeae941b833c1d8e6952b18b7d4c54e7
Brittany Rowe <rowebrittany@gmail.com>
2017-11-14 16:30:09 -0700

    Added models

---
+++ BlowOut/Models/Client.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BlowOut.Models
+{
+    [Table("Client")]
+
+    public class Client
+    {
+            [Key]
+            public int clientID { get; set; }
+            [Required]
+            public String firstName { get; set; }
+            [Required]
+            public String lastName { get; set; }
+            [Required]
+            public String address { get; set; }
+            [Required]
+            public String city { get; set; }
+            [Required]
+            public String state { get; set; }
+            [Required (ErrorMessage ="Please make sure the ZIP code has 5 digits"]
+            public int zip { get; set; }
+            [Required (ErrorMessage ="Please enter your email address in the following format: test@example.com"]
+            public String emailAddress { get; set; }
+            [Required]
+            public int phone { get; set; }
+
+        
+    }
+}
---
+++ BlowOut/Models/Instrument.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BlowOut.Models
+{
+    [Table("Instrument")]
+    public class Instrument
+    {
+        [Key]
+        public int instrumentID { get; set; }
+        public int clientID { get; set; }
+
+        [Required(ErrorMessage = "Please enter an instrument description")]
+        public String instrumentDesc { get; set; }
+        public String type { get; set; }
+        [Required(ErrorMessage = "Please enter a price")]
+        public decimal? price { get; set; }
+    }
+}
